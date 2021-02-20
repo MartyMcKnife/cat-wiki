7d7463525943d2f8568d97307d2b339ca02f1095
Marty McKnife <58376595+MartyMcKnife@users.noreply.github.com>
2021-02-20 18:01:50 +0800

    Attempt to fix oversize fontawesome icon

--- components/Hero/Search.tsx
+++ components/Hero/Search.tsx
@@ -73,6 +73,7 @@ export default function Search({ cats }: Props): ReactElement {
           icon={faSearch}
           color="#291507"
           className="mx-auto mr-5"
+          size="1x"
         />
       </div>
 
